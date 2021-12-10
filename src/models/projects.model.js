77f271bd4c69bf9e141f1ac4dd9bcc7af45122ea
olivier.francey <olivier.francey@mbpdeolrfrancey.home>
2021-12-11 00:19:42 +0100

    projects service

---
+++ src/models/projects.model.js
@@ -0,0 +1,24 @@
+// projects-model.js - A mongoose model
+// 
+// See http://mongoosejs.com/docs/models.html
+// for more of what you can do here.
+module.exports = function (app) {
+  const modelName = 'projects';
+  const mongooseClient = app.get('mongooseClient');
+  const { Schema } = mongooseClient;
+  const schema = new Schema({
+    name: { type: String },
+    number: { type: String, required: true },
+    description: { type: String }
+  }, {
+    timestamps: true
+  });
+
+  // This is necessary to avoid model compilation errors in watch mode
+  // see https://mongoosejs.com/docs/api/connection.html#connection_Connection-deleteModel
+  if (mongooseClient.modelNames().includes(modelName)) {
+    mongooseClient.deleteModel(modelName);
+  }
+  return mongooseClient.model(modelName, schema);
+  
+};
