1faf5b22dd42669a22d77ff98d2396733d8d885c
olivier.francey <olivier.francey@mbpdeolrfrancey.home>
2021-12-11 00:29:16 +0100

    service index.js projects

--- src/services/index.js
+++ src/services/index.js
@@ -1,7 +1,9 @@
 const users = require('./users/users.service.js');
 const documents = require('./documents/documents.service.js');
+const projects = require('./projects/projects.service.js');
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
   app.configure(users);
   app.configure(documents);
+  app.configure(projects);
 };
